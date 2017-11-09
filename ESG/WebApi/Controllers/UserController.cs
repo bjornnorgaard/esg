e069515b0422c3c8f18608b7c1d15e150dfc8e0a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-11-09 19:32:33 +0100

    Added get method for getting user by id

--- ESG/WebApi/Controllers/UserController.cs
+++ ESG/WebApi/Controllers/UserController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
     [Route("api/user")]
     public class UserController : Controller
     {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation(nameof(UserController) + " is working!");
             return Ok($"{nameof(UserController)} is working!");
+        }
+
+        [HttpGet, Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            _logger.LogDebug("User with {UserId} requested", id);
+
+            var user = new User(id);
+            _logger.LogInformation("{@User} found with {UserId}", user, id);
+
+            return Json(user);
         }
     }
 }
--- ESG/WebApi/Models/User.cs
+++ ESG/WebApi/Models/User.cs
@@ -1,9 +1,20 @@
-﻿namespace WebApi.Models
+﻿using System;
+
+namespace WebApi.Models
 {
     public class User
     {
+        public User(int id)
+        {
+            Id = id;
+            Name = "John Doe";
+            Age = new Random().Next(18, 80);
+            Birthdate = DateTime.Now.Subtract(new TimeSpan(Age, 0, 0));
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
     }
 }
