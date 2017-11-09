7dd984407f1a094e8619477de413b58a8c213272
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-11-09 19:19:22 +0100

    Installed swagger

--- ESG/WebApi/Startup.cs
+++ ESG/WebApi/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApi
 {
@@ -18,6 +20,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(conf => conf.SwaggerDoc("v1", new Info { Title = "API", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +32,9 @@ namespace WebApi
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(conf => conf.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 API"));
+            app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
         }
     }
 }
