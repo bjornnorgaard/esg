f9949e7aef846dfca4b8277a468db6010b63298d
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-11-09 19:14:17 +0100

    Usings cleanup

---
+++ ESG/WebApi/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"{nameof(UserController)} is working!");
+        }
+    }
+}
