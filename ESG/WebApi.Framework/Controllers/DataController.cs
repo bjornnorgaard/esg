77abb0af98de83d751dd86c093ef73f07ca79e9e
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-11-11 20:11:29 +0100

    Setup framwork projekct to test serilog without core

---
+++ ESG/WebApi.Framework/Controllers/DataController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Framework.Controllers
+{
+    public class DataController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Json($"{nameof(DataController)} is working!");
+        }
+    }
+}
